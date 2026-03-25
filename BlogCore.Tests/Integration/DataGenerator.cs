5c864be201bbfdbd36c93c3b945f104f4edf4306
Filip <archaikek>
2026-03-25 23:55:51 +0100

    Reverting indentation changes because apparently that's not what Big Linter wanted anyway

--- BlogCore.Tests/Integration/DataGenerator.cs
+++ BlogCore.Tests/Integration/DataGenerator.cs
@@ -9,18 +9,18 @@ using System.Threading.Tasks;
 
 namespace BlogCore.Tests.Integration
 {
-    public static class DataGenerator
-    {
-        public static Faker<Post> GetPostFaker() =>
-            new Faker<Post>()
-            //.RuleFor(p => p.Id, f => f.UniqueIndex)
-            .RuleFor(p => p.Author, f => f.Name.FullName())
-            .RuleFor(p => p.Content, f => f.Lorem.Paragraphs(1));
+	public static class DataGenerator
+	{
+		public static Faker<Post> GetPostFaker() =>
+			new Faker<Post>()
+			//.RuleFor(p => p.Id, f => f.UniqueIndex)
+			.RuleFor(p => p.Author, f => f.Name.FullName())
+			.RuleFor(p => p.Content, f => f.Lorem.Paragraphs(1));
 
-        public static Faker<Comment> GetCommentFaker(int postId) =>
-            new Faker<Comment>()
-            //.RuleFor(c => c.Id, f => f.UniqueIndex)
-            .RuleFor(c => c.PostId, _ => postId)
-            .RuleFor(c => c.Content, f => f.Lorem.Sentence());
-    }
+		public static Faker<Comment> GetCommentFaker(int postId) =>
+			new Faker<Comment>()
+			//.RuleFor(c => c.Id, f => f.UniqueIndex)
+			.RuleFor(c => c.PostId, _ => postId)
+			.RuleFor(c => c.Content, f => f.Lorem.Sentence());
+	}
 }
--- BlogCore.Tests/Unit/CommentTest.cs
+++ BlogCore.Tests/Unit/CommentTest.cs
@@ -13,72 +13,72 @@ using Org.BouncyCastle.Asn1.X509;
 
 namespace BlogCore.Tests.Unit
 {
-    [TestClass]
-    public class CommentTest : IntegrationTestBase
-    {
-        [TestMethod]
-        public async Task AddComment_ValidData_IncreasesCountForPost()
-        {
-            // Arrange:
-            var newPost = DataGenerator.GetPostFaker().Generate();
-            _ctx.Posts.Add(newPost);
-            await _ctx.SaveChangesAsync();
+	[TestClass]
+	public class CommentTest : IntegrationTestBase
+	{
+		[TestMethod]
+		public async Task AddComment_ValidData_IncreasesCountForPost()
+		{
+			// Arrange:
+			var newPost = DataGenerator.GetPostFaker().Generate();
+			_ctx.Posts.Add(newPost);
+			await _ctx.SaveChangesAsync();
 
-            var newComment = DataGenerator.GetCommentFaker(newPost.Id).Generate();
-            _ctx.Comments.Add(newComment);
-            await _ctx.SaveChangesAsync();
+			var newComment = DataGenerator.GetCommentFaker(newPost.Id).Generate();
+			_ctx.Comments.Add(newComment);
+			await _ctx.SaveChangesAsync();
 
-            // Act:
-            var result = _repo.GetCommentsByPostId(newPost.Id).Count();
+			// Act:
+			var result = _repo.GetCommentsByPostId(newPost.Id).Count();
 
-            // Assert:
-            Assert.AreEqual(result, 1);
-        }
+			// Assert:
+			Assert.AreEqual(result, 1);
+		}
 
-        [TestMethod]
-        public async Task GetCommentsByPostId_NonExistentPost_ReturnsEmpty()
-        {
-            // Arrange:
+		[TestMethod]
+		public async Task GetCommentsByPostId_NonExistentPost_ReturnsEmpty()
+		{
+			// Arrange:
 
-            // Act:
-            var result = _repo.GetCommentsByPostId(2221);
+			// Act:
+			var result = _repo.GetCommentsByPostId(2221);
 
-            // Assert:
-            Assert.IsTrue(result.IsNullOrEmpty());
-        }
+			// Assert:
+			Assert.IsTrue(result.IsNullOrEmpty());
+		}
 
-        [TestMethod]
-        [ExpectedException(typeof(Microsoft.EntityFrameworkCore.DbUpdateException))]
-        public async Task AddComment_OrphanComment_ThrowsException()
-        {
-            // Arrange:
-            var badComment = DataGenerator.GetCommentFaker(2221).Generate();
+		[TestMethod]
+		[ExpectedException(typeof(Microsoft.EntityFrameworkCore.DbUpdateException))]
+		public async Task AddComment_OrphanComment_ThrowsException()
+		{
+			// Arrange:
+			var badComment = DataGenerator.GetCommentFaker(2221).Generate();
 
-            // Act:
-            _repo.AddComment(badComment);
+			// Act:
+			_repo.AddComment(badComment);
 
-            // Assert:
-        }
+			// Assert:
+		}
 
-        [TestMethod]
-        public async Task MultipleComments_DifferentPosts_ReturnsOnlyCorrectOnes()
-        {
-            // Arrange:
-            var posts = DataGenerator.GetPostFaker().Generate(2);
-            _ctx.Posts.AddRange(posts);
-            await _ctx.SaveChangesAsync();
+		[TestMethod]
+		public async Task MultipleComments_DifferentPosts_ReturnsOnlyCorrectOnes()
+		{
+			// Arrange:
+			var posts = DataGenerator.GetPostFaker().Generate(2);
+			_ctx.Posts.AddRange(posts);
+			await _ctx.SaveChangesAsync();
 
-            var comments1 = DataGenerator.GetCommentFaker(posts[0].Id).Generate(5);
-            var comments2 = DataGenerator.GetCommentFaker(posts[1].Id).Generate(2);
-            _ctx.Comments.AddRange(comments1);	
-            _ctx.Comments.AddRange(comments2);
-            await _ctx.SaveChangesAsync();
+			var comments1 = DataGenerator.GetCommentFaker(posts[0].Id).Generate(5);
+			var comments2 = DataGenerator.GetCommentFaker(posts[1].Id).Generate(2);
+			_ctx.Comments.AddRange(comments1);	
+			_ctx.Comments.AddRange(comments2);
+			await _ctx.SaveChangesAsync();
 
-            // Act:
-            var result = _repo.GetCommentsByPostId(posts[0].Id);
+			// Act:
+			var result = _repo.GetCommentsByPostId(posts[0].Id);
 
-            // Assert:
-            Assert.IsFalse(result.Contains(comments2[0]) || result.Contains(comments2[1]));
-        }
-    }
+			// Assert:
+			Assert.IsFalse(result.Contains(comments2[0]) || result.Contains(comments2[1]));
+		}
+	}
 }
